14c9cfca141546d348c484b9c27cc1e784eaec63
Ureka Gautam <urekagautam19@gmail.com>
2026-05-15 15:55:09 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="h-screen bg-amber-100 flex items-center justify-center">
+      <h1 className="text-7xl font-bold text-orange-500">
+        Testing Tailwind - BCA 8th Sem
+      </h1>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
